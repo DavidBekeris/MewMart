effa648de764be60fc14d14147b30fbaf3f1e686
DavidBekeris <david-_-bkrs@hotmail.com>
2025-09-13 15:10:25 +0200

    Added connection to SQL DB. Created Category DB.

---
+++ MewMartWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MewMartWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MewMartWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
