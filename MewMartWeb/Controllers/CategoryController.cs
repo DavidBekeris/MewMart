bad4f4c689ac6b5ed957ce4251aaedcc792707ae
DavidBekeris <david-_-bkrs@hotmail.com>
2025-09-15 14:10:11 +0200

    Added function to controller to retrieve categories from db

--- MewMartWeb/Controllers/CategoryController.cs
+++ MewMartWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MewMartWeb.Data;
+using MewMartWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MewMartWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
