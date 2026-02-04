806e14a1beb6fba692a7085fc0d0cb2b038ae35c
DavidBekeris <david-_-bkrs@hotmail.com>
2026-02-04 10:57:39 +0100

    Added Dropdown menu button. Added model for product.

---
+++ MewMart.Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MewMart.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Manufacturer { get; set; }
+        [Required]
+        [Display(Name = "List Pris")]
+        [Range (1,1000)]
+        public double ListPrize { get; set; }
+        [Required]
+        [Display(Name = "Pris")]
+        [Range(1, 1000)]
+        public double Prize { get; set; }
+        [Required]
+        [Display(Name = "Pris för 2-4")]
+        [Range(1, 1000)]
+        public double Prize2 { get; set; }
+        [Required]
+        [Display(Name = "Pris för 5+")]
+        [Range(1, 1000)]
+        public double Prize5 { get; set; }
+    }
+}
